92eb9a7c62af7a1299c1c9eef4a82f37f60dc025
minhazapon <minhajulabedinapon@gmail.com>
2024-05-22 09:47:55 -0700

    footer files

---
+++ src/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+
+
+
+
+
+const Contact = () => {
+    return (
+        <div className=" mb-10 mt-32 ml-28 mr-28">
+
+            <div>
+
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+                       
+        </div>
+    );
+};
+
+export default Contact;
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,9 +7,36 @@ const Footer = () => {
 
             <div>
 
-
+            
+                        <footer className="footer p-10 bg-base-200 text-base-content">
+              <aside>
+                <img className=" h-[50px]" src="https://i.ibb.co/Twwhq9J/coding.png" alt="" />
+                <p className=" text-xl">Full Name: Minahzul abedin Apon<br/>  <span className=" text-2xl font-bold">Front End <span className=" text-blue-500">React</span> Developer</span></p>
+              </aside> 
+              <nav>
+                <h6 className="footer-title">Social</h6> 
+                <div className=' flex items-center gap-2'>
+     <img className=' h-[30px]' src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/800px-Facebook_logo_%28square%29.png" alt="" /> 
+    <a className="link link-hover">Facebook</a>
+    </div>
+    <div className=' flex items-center gap-2'>
+      <img className=' h-[30px]' src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png?20140125013055" alt="" />
+    <a className="link link-hover">Linkedin</a>
+    </div>
+    <div className=' flex items-center gap-2'>
+      <img className=' h-[30px]' src="https://i.ibb.co/1Kt0NDr/github-1.png" alt="" />
+    <a className="link link-hover">GitHub</a>
+    </div>
+              </nav> 
+              <nav>
+                <h6 className="footer-title">Conatct With Me</h6> 
+                <a className="link link-hover text-xl ">Email: <span className=" font-bold">minhazaponcodingwarrior@gmail.com</span>
+                </a>
+                <a className="link link-hover text-xl ">Call Me: <span className=" font-bold">+8801950374409</span></a>
                
-
+              </nav> 
+            </footer>
+            
 
 
 
